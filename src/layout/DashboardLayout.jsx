99379549a6d0d6d06ff2856acc9e808c27dc627a
Ashik465 <helloashikul@gmail.com>
2023-06-09 15:43:45 +0600

    my classes half done

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -12,7 +12,7 @@ const DashboardLayout = () => {
 
         <div className="drawer lg:drawer-open ">
   <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-  <div className="drawer-content flex flex-col items-center justify-center">
+  <div className="drawer-content flex flex-col items-center justify-center overflow-x-auto">
     {/* Page content here */}
     <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
 
--- src/pages/dashboard/myclasses/MyClasses.jsx
+++ src/pages/dashboard/myclasses/MyClasses.jsx
@@ -8,7 +8,54 @@ const MyClasses = () => {
       <Helmet>
         <title>EliteSports Academy | My Classes</title>
       </Helmet>
-      <h1>My Classes {classes.length}</h1>
+       {/* // table start  */}
+
+       <div className="overflow-x-auto p-10 w-full">
+        <table className="table w-full ">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Class image </th>
+              <th>Status</th>
+              <th>Total Enrolled Student</th>
+              <th>Feedback</th>
+              <th>Update</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row  */}
+            {classes.map((item, index) => (
+              <tr key={item._id}>
+                <td>{index + 1}</td>
+                <td>
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img
+                        src={item?.
+                          classImg
+                          }
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  {item?.status} 
+                </td>
+                <td >{item?.
+enrolledStudents}</td>
+                <td>
+                  {item?.feedback}
+                </td>
+                <td>
+                  <button  className="btn btn-success   text-white">update</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
