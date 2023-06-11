87e7ec1178a489234fa90cef29eba73486626d70
Ashik465 <helloashikul@gmail.com>
2023-06-11 15:53:30 +0600

    popular classes animation done

--- src/pages/home/popularClass/PopularClasses.jsx
+++ src/pages/home/popularClass/PopularClasses.jsx
@@ -1,12 +1,10 @@
 import { useEffect, useState } from "react";
-
+import { motion } from "framer-motion";
 
 const PopularClasses = () => {
+  const [popularClasses, setPopularClasses] = useState([]);
 
-    const [popularClasses, setPopularClasses] = useState([]);
-  
-
-  //  fetch class data from database
+  // Fetch class data from the database
   useEffect(() => {
     fetch("http://localhost:5000/classes/popularClasses")
       .then((res) => res.json())
@@ -15,66 +13,61 @@ const PopularClasses = () => {
       });
   }, []);
 
-    return (
-        <> <div className=" text-center ">
-        <h2 className="text-4xl  text-black text-center font-bold my-10 bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">
-            Popular Classes
-      </h2> 
-        </div>
-        {/* // card  */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-10 py-20 px-5 bg-lime-100 ">
+  return (
+    <>
+      <div className="text-center">
+        <h2 className="text-4xl text-black text-center font-bold my-10 bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">
+          Popular Classes
+        </h2>
+      </div>
+
+      {/* Card */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-10 py-20 px-5 bg-lime-100">
         {popularClasses?.map((classess) => (
-          <div
+          <motion.div
             key={classess?._id}
-            className={`card   my-5 bg-base-100 rounded-3xl shadow-xl  ${classess?.availableSeats===0 &&  "bg-red-600"}`}
+            className={`card my-5 bg-base-100 rounded-3xl shadow-xl ${
+              classess?.availableSeats === 0 && "bg-red-600"
+            }`}
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            transition={{ duration: 0.2 }}
           >
             <figure className="px-10 pt-10">
               <img
-              
                 src={classess?.classImg}
                 alt="class"
                 className="rounded-3xl h-96 w-full object-cover"
               />
             </figure>
-            <div className="card-body ">
+            <div className="card-body">
               <h2 className="card-title">
-                {" "}
-                <span className="text-[#AC9C63]  font-bold">Class Name:</span>
-               <span className=" uppercase"> {classess?.className}</span>
+                <span className="text-[#AC9C63] font-bold">Class Name:</span>
+                <span className="uppercase">{classess?.className}</span>
               </h2>
               <p>
-                <span className="text-[#AC9C63]  font-bold">Instructors :</span>{" "}
+                <span className="text-[#AC9C63] font-bold">Instructors :</span>{" "}
                 {classess?.instructorName}
               </p>
               <p>
-                {" "}
-                <span className="text-[#AC9C63]  font-bold">
-                  Available Seats:
-                </span>{" "}
+                <span className="text-[#AC9C63] font-bold">Available Seats:</span>{" "}
                 {classess?.availableSeats}
               </p>
               <p>
-                {" "}
-                <span className=" text-[#AC9C63] font-bold">Price:</span> $
+                <span className="text-[#AC9C63] font-bold">Price:</span> $
                 {classess?.price}
               </p>
               <p>
-                {" "}
-                <span className=" text-[#AC9C63] font-bold">Enroll Students: </span> 
+                <span className="text-[#AC9C63] font-bold">Enroll Students:</span>{" "}
                 {classess?.enrolledStudents}
               </p>
-
-              <div className="card-actions">
-               
-              </div>
+              <div className="card-actions"></div>
             </div>
-          </div>
+          </motion.div>
         ))}
       </div>
-        
-        </>
-       
-    );
+    </>
+  );
 };
 
 export default PopularClasses;
