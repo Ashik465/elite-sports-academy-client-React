87347c9039cc756abca2d56a2ad1d76a1d63093a
Ashik465 <helloashikul@gmail.com>
2023-06-17 15:51:57 +0600

    banner auto play done

--- src/pages/home/banner/Slider.jsx
+++ src/pages/home/banner/Slider.jsx
@@ -3,9 +3,8 @@ import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
-
-// import required modules
-import { Navigation } from "swiper";
+// Import Swiper modules
+import { Navigation, Autoplay } from "swiper";
 
 // import custom css 
 import './Slider.css'
@@ -17,54 +16,64 @@ import { Link } from "react-router-dom";
 
 const Slider = () => {
 
-    return (
-        <>
-            <div className="swiper ">
-            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide><div className="slide-content">
-          <img src={banner1} alt="" />
-          <div className="overlay-text p-5 md:p-20 ">
-            <h3 className=" text-xl md:text-7xl font-bold md:mb-4 uppercase">This Summer join Us!</h3>
-            <p className="hidden md:block">Get Your performance journey started at EliteSports Academy with a free sports class.we offer a world class training experiences.Reach your full potential at  EliteSports Academy  </p>
-<Link to = '/classes'> <button className="btn  border-[#AC9C63] border-2 rounded-none bg-black text-white hover:bg-[#AC9C63] 
-           md: mt-4 bg-opacity-20 hover:opacity-100 
-        ">
-              Start Your free trial
-            </button></Link>
-           
-          </div>
-        </div></SwiperSlide>
-        <SwiperSlide> <div className="slide-content">
-          <img src={banner2} alt="" />
-          <div className="overlay-text p-5 md:p-20 ">
-            <h3 className=" text-xl md:text-7xl font-bold md:mb-4 uppercase"> join a membership program!</h3>
-            <p className="hidden md:block"> EliteSports Academy membership offer something for every athlete.we offer a world class training experiences.Reach your full potential at  EliteSports Academy  </p>
-            <Link to = '/classes'><button className="btn  border-[#AC9C63] border-2 rounded-none bg-black text-white hover:bg-[#AC9C63] 
-           md: mt-4 bg-opacity-20 hover:opacity-100 
-        ">
-              Join today
-            </button></Link>
-            
-          </div>
-        </div></SwiperSlide>
-        <SwiperSlide> <div className="slide-content">
-          <img src={banner3} alt="" />
-          <div className="overlay-text p-5 md:p-20 ">
-            <h3 className=" text-xl md:text-7xl font-bold md:mb-4 uppercase">Try a Free Sports Class!</h3>
-            <p className="hidden md:block">Get Your performance journey started at EliteSports Academy with a free sports class.we offer a world class training experiences.Reach your full potential at  EliteSports Academy  </p>
-            <Link to = '/classes'><button className="btn  border-[#AC9C63] border-2 rounded-none bg-black text-white hover:bg-[#AC9C63] 
-           md: mt-4 bg-opacity-20 hover:opacity-100 
-        ">
-              Start Your free trial
-            </button></Link>
-            
-          </div>
-        </div></SwiperSlide>
-        
-      </Swiper>
+  return (
+    <>
+      <div className="swiper ">
+        <Swiper
+          navigation={true}
+          modules={[Navigation, Autoplay]} // Include Autoplay module
+          className="mySwiper"
+          autoplay={{ delay: 3000 }} // Set autoplay delay (in milliseconds)
+        >
+          <SwiperSlide>
+            <div className="slide-content">
+              <img src={banner1} alt="" />
+              <div className="overlay-text p-5 md:p-20 ">
+                <h3 className="text-xl md:text-7xl font-bold md:mb-4 uppercase">This Summer join Us!</h3>
+                <p className="hidden md:block">Get Your performance journey started at EliteSports Academy with a free sports class.we offer a world class training experiences.Reach your full potential at EliteSports Academy</p>
+                <Link to='/classes'>
+                  <button className="btn  border-[#AC9C63] border-2 rounded-none bg-black text-white hover:bg-[#AC9C63] 
+           md: mt-4 bg-opacity-20 hover:opacity-100">
+                    Start Your free trial
+                  </button>
+                </Link>
+              </div>
             </div>
-        </>
-    );
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="slide-content">
+              <img src={banner2} alt="" />
+              <div className="overlay-text p-5 md:p-20 ">
+                <h3 className="text-xl md:text-7xl font-bold md:mb-4 uppercase"> join a membership program!</h3>
+                <p className="hidden md:block"> EliteSports Academy membership offer something for every athlete.we offer a world class training experiences.Reach your full potential at EliteSports Academy</p>
+                <Link to='/classes'>
+                  <button className="btn  border-[#AC9C63] border-2 rounded-none bg-black text-white hover:bg-[#AC9C63] 
+           md: mt-4 bg-opacity-20 hover:opacity-100">
+                    Join today
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="slide-content">
+              <img src={banner3} alt="" />
+              <div className="overlay-text p-5 md:p-20 ">
+                <h3 className="text-xl md:text-7xl font-bold md:mb-4 uppercase">Try a Free Sports Class!</h3>
+                <p className="hidden md:block">Get Your performance journey started at EliteSports Academy with a free sports class.we offer a world class training experiences.Reach your full potential at EliteSports Academy</p>
+                <Link to='/classes'>
+                  <button className="btn  border-[#AC9C63] border-2 rounded-none bg-black text-white hover:bg-[#AC9C63] 
+           md: mt-4 bg-opacity-20 hover:opacity-100">
+                    Start Your free trial
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </>
+  );
 };
 
 export default Slider;
