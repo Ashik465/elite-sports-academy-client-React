678750e65794f59e11644b8a5c564dc49b8e1f02
Ashik465 <helloashikul@gmail.com>
2023-06-17 16:57:33 +0600

    loader updated

--- src/loader/LoadingSpiner.jsx
+++ src/loader/LoadingSpiner.jsx
@@ -1,9 +1,11 @@
+import { ScaleLoader } from "react-spinners";
 
 
 const LoadingSpiner = () => {
     return (
         <div className="flex items-center justify-center h-screen">
-        <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-[#AC9C63] "></div>
+        {/* <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-[#AC9C63] "></div> */}
+        <div className=" h-16 w-16"><ScaleLoader color="#AC9C63" /></div>
       </div>
     );
 };
